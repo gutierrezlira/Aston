a456c9da844191e967dbe932adf893ba226d633d
AlinaDarenskikh <dawmond@mail.ru>
2023-03-28 12:53:21 +0300

    Second task created Task2_Greeting

---
+++ src/main/java/Task2_Greeting.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task2_Greeting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите имя: ");
+        String name = scanner.nextLine();
+        greetUser(name);
+    }
+
+    private static void greetUser(String name) {
+        if (name.equalsIgnoreCase("вячеслав")) {
+            System.out.println("Привет, Вячеслав");
+        } else {
+            System.out.println("Нет такого имени");
+        }
+    }
+}
